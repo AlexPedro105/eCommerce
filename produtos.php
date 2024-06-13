3c451d4b4a83289eb7c808dafe5829ee4c3ca23a
Alex Pedro <alexps0123@gmail.com>
2024-06-12 22:14:50 -0300

    Desenvolvimentos dos filtros
    Esse commit vem para subir os filtros de Preço e Estoque na tela de Produtos.

--- produtos.php
+++ produtos.php
@@ -12,20 +12,35 @@
                 <ul class="dropdown-menu">
                     <li class="dropdown-header">
                         <p class="selecionaProdutos">
-                            <span>12 </span> Selecionados 
-                            <a class="selecionaProdutosReiniciar"> Reiniciar </a> 
-                        </p> 
+                            <span>12 </span> Selecionados
+                            <a class="selecionaProdutosReiniciar"> Reiniciar </a>
+                        </p>
                     </li>
                     <li role="separator" class="divider"></li>
                     <li>
-                        <p class="estoqueProdutos"><input class="form-check-input" type="checkbox" value="emEstoque"> Em estoque <span> 12</span> </p> 
+                        <div class="estoqueProdutos form-check"><input class="form-check-input" type="checkbox" value="emEstoque"> Em estoque <span> 12</span> </div>
                     </li>
                     <li>
-                       <p class="estoqueProdutos"> <input class="form-check-input" type="checkbox" value="">  Fora de estoque  <span> 12</span></p>
+                        <p class="estoqueProdutos"> <input class="form-check-input" type="checkbox" value=""> Fora de estoque <span> 12</span></p>
                     </li>
                 </ul>
             </span>
-            <a class="filtrosProdutos">Preço: </a>
+            <span class="filtrosProdutos"><a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Preço: </a>
+                <ul class="dropdown-menu dropdownPreco">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <li class="dropdown-header">
+                                    <a class="maiorPrecoReiniciar"> Reiniciar</a>
+                            </li>
+                        </div>
+                    </div>
+                    <li>
+                        <p class="precoDePara"><label class="form-label"> R$: </label> <input class="form-control" type="text" value="" placeholder="De ">
+                           <label class="form-label"> R$: </label> <input class="form-control" type="text" placeholder="Para " value=""></p>
+                    </li>
+                </ul>
+            </span>
         </div>
     </div>
 </div>
