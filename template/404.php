6bf7427ea56a7bfb78fd90775935aa784118f66c
Alex Pedro <alexps0123@gmail.com>
2024-09-18 21:38:33 -0300

    Desenvolvimento da Tela 404
    Esse commit é para desenvolver a tela 404 para utilização de telas inexistentes.

---
+++ template/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/estilo.css">
+	<title>404</title>
+</head>
+<body>
+	<div class="container">
+		<img  class="img-fluid img404" alt="Responsive image" src="../img/undraw_page_not_found_re_e9o6.png">
+	</div>
+</body>
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+</html>
